7ffb0c1ccc43a7e9a205345fd4dec78bc15011a4
freemankevin <freemankevin2017@gmail.com>
2026-01-26 18:17:55 +0800

    update files.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import tailwind from '@astrojs/tailwind';
+import vercel from '@astrojs/vercel/static';
 
 export default defineConfig({
   integrations: [mdx(), tailwind()],
+  output: 'static',
+  adapter: vercel(),
   site: 'https://cat.freemankevin.uk',
   base: '',
   devToolbar: {
